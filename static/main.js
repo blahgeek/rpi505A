490c51a131f59455e6d542d3debd7f38b0072333
BlahGeek <blahgeek@gmail.com>
2014-02-21 14:36:11 +0800

    fix

--- static/main.js
+++ static/main.js
@@ -17,28 +17,27 @@ $(document).ready(function(){
         $.post(base_url + '/speak', {content: content});
     });
 
-    var update_switch_status = function(){
-        $.get(base_url + '/lightstatus', function(data){
-            var $switch = $('#switch');
-            $switch.data('on', data);
-            if(data == '1')
-                $switch.text('Turn Off Light');
-            else
-                $switch.text('Turn On Light');
-        });
+    var update_switch_status = function(data){
+        var $switch = $('#switch');
+        var d = parseInt(data);
+        $switch.data('on', d);
+        if(d == 1)
+            $switch.text('Turn Off Light');
+        else
+            $switch.text('Turn On Light');
     };
 
     $('#switch').click(function(e){
         e.preventDefault();
         var $switch = $('#switch');
         var url = base_url;
-        if($switch.data('on') == '1')
+        if($switch.data('on') == 1)
             url = base_url + '/turnoff505A';
         else
             url = base_url + '/turnon505A';
         $.get(url, update_switch_status);
     });
 
-    update_switch_status();
+    $.get(base_url + '/lightstatus', update_switch_status);
     
 });
